663116d4f6cb533ccdb654156618b0f7b53e4afc
Dawid <u56128@student.wat.edu.pl>
2025-10-02 19:39:55 +0200

    Modyfikuje szablon

--- Exercises_And_Practice/amazon-project-bymself/code/amazon.js
+++ Exercises_And_Practice/amazon-project-bymself/code/amazon.js
@@ -1,10 +1,14 @@
-document.querySelectorAll('.js-select-button').forEach((buttonNr) => {
+// Generowanie wartosci dla wybierania ilosci produktow
+
+document.querySelectorAll('.js-amazon-select-button').forEach((buttonNr) => {
     let html = ``;
     for (let i = 1; i<=10; i++) {
         html += `<option value="${i}">${i}</option>\n`;
     }
     buttonNr.innerHTML = html;
 });
+
+////////////////////////////////////////////////////////////
 
 
 
